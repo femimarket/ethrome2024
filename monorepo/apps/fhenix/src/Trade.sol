082d38a18a1127ae4763652cd0133d9034d1902b
u <you@example.com>
2024-10-05 19:04:23 +0200

    feat: use fhenix encryption

--- monorepo/apps/fhenix/src/Trade.sol
+++ monorepo/apps/fhenix/src/Trade.sol
@@ -7,17 +7,19 @@ import { Math } from "./Math.sol";
 
 contract Trade {
 
-    event NewTick(string asset, int64 price);
+    event NewTick(string asset, euint32 price);
 
     address public owner;
     struct TradeInfo {
-        int64 price;
-        int64 qty;
-        int64 entry;
-        int64 pl;
+        euint32 price;
+        euint32 qty;
+        euint32 entry;
+        euint32 p;
+        euint32 l;
+        euint32 tp;
+        euint32 tl;
         uint256 timestamp;
         ebool buy;
-        bool init;
     }
     mapping(string => TradeInfo) public trades;
 
@@ -30,48 +32,72 @@ contract Trade {
         _;
     }
 
-    function getPl(string memory asset) public view returns (int64) {
-        return trades[asset].pl;
+    function getPl(string memory asset) public view returns (euint32,euint32) {
+        return (trades[asset].p, trades[asset].l);
     }
 
     
 
-    function addPrice(string memory asset, int64 price) public {
+    function addPrice(string memory asset, inEuint32 calldata price) public {
         
         TradeInfo storage trade =  trades[asset];
 
-        emit NewTick(asset, price);
+        emit NewTick(asset, FHE.asEuint32(price));
 
-        trade.price = price;
+        trade.price = FHE.asEuint32(price);
         trade.timestamp = block.timestamp;
 
-        
-        if (trade.init == true) {
-            int64 pl = (price - trade.entry)*trade.qty;
-            trade.pl =  pl;
-        }
+
+        euint32 min = FHE.min(trade.entry, trade.price);   
+        euint32 max = FHE.max(trade.entry, trade.price);   
+        euint32 diff = max - min;
+        euint32 pl = diff * trade.qty;
+
+        euint8 win1 = FHE.select(FHE.lt(trade.price, trade.entry), FHE.asEuint8(1), FHE.asEuint8(0));
+        euint8 win2 = FHE.select(trade.buy.eq(FHE.asEbool(false)), FHE.asEuint8(1),  FHE.asEuint8(0));
+        euint8 win3 = FHE.select(FHE.gt(trade.price, trade.entry), FHE.asEuint8(1),  FHE.asEuint8(0));
+        euint8 win4 = FHE.select(FHE.eq(trade.buy,FHE.asEbool(true)), FHE.asEuint8(1),  FHE.asEuint8(0));
+        euint8 win = win1 + win2 + win3 + win4;
+        trade.tp = trade.p + FHE.select(win.eq(FHE.asEuint8(1)), pl, trade.tp);
+        trade.tl = trade.l + FHE.select(win.eq(FHE.asEuint8(0)), pl, trade.tl);
     }
 
-    function addTrade(string memory asset, int64 _qty) public {
-         
+
+    function enterTrade(string memory asset, inEuint32 calldata _qty, ebool _buy) public {
         TradeInfo storage trade =  trades[asset];
    
         if (trade.timestamp == 0) {
             revert("Asset not initialized.");
         }
      
-        int64 qty = _qty;
-
-        if (trade.init == false) {
-            trade.qty = qty;
-            trade.init = true;
-        } else {
-            int64 pl = (trade.price - trade.entry) * trade.qty;
-            trade.pl = trade.pl + pl;
-            trade.qty = trade.qty + qty;
-        }
-
+        euint32 qty = FHE.asEuint32(_qty);
+        trade.qty = qty;
+        trade.buy = _buy;
         trade.entry = trade.price;
+    }
+
+
+    function exitTrade(string memory asset) public {
+        TradeInfo storage trade =  trades[asset];
+   
+
+        euint32 min = FHE.min(trade.entry, trade.price);   
+        euint32 max = FHE.max(trade.entry, trade.price);   
+        euint32 diff = max - min;
+        euint32 pl = diff * trade.qty;
+
+
+        euint8 win1 = FHE.select(FHE.lt(trade.price, trade.entry), FHE.asEuint8(1), FHE.asEuint8(0));
+        euint8 win2 = FHE.select(FHE.eq(trade.buy,FHE.asEbool(false)), FHE.asEuint8(1),  FHE.asEuint8(0));
+        euint8 win3 = FHE.select(FHE.gt(trade.price, trade.entry), FHE.asEuint8(1),  FHE.asEuint8(0));
+        euint8 win4 = FHE.select(FHE.eq(trade.buy,FHE.asEbool(true)), FHE.asEuint8(1),  FHE.asEuint8(0));
+        euint8 win = win1 + win2 + win3 + win4;
+
+
+        trade.p = trade.p + FHE.select(win.eq(FHE.asEuint8(1)), pl, trade.p);
+        trade.l = trade.l + FHE.select(win.eq(FHE.asEuint8(0)), pl, trade.l);
+        trade.qty = FHE.asEuint32(0);
+        trade.entry = FHE.asEuint32(0);
     }
 
 }
